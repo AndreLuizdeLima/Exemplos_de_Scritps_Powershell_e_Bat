38960ad961d7795251554a9860f2f7f0341c8c8b
Andre <andreluizjp2015@gmail.com>
2024-02-14 15:44:40 -0300

    14020224 1

--- projeto_prepara/script_preparacao_sistema.ps1
+++ projeto_prepara/script_preparacao_sistema.ps1
@@ -1,7 +1,13 @@
 Add-Type -assembly System.Windows.Forms
 
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $arguments = "& '" + $MyInvocation.MyCommand.Definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    exit
+}
+
 $main_form = New-Object System.Windows.Forms.Form
-$main_form.Text ='Script de Preparacao de Ambiente'
+$main_form.Text ='Script de Utilitarios'
 
 $main_form.Width = 900
 $main_form.Height = 600
@@ -9,8 +15,8 @@ $main_form.AutoSize = $true
 $main_form.BackColor = [System.Drawing.Color]::FromArgb(17, 17, 17)
 
 $Label1 = New-Object System.Windows.Forms.Label
-$Label1.Text = "Monitoramente do Sistema"
-$Label1.Location = New-Object System.Drawing.Point(20,30)
+$Label1.Text = "Selecione o que deseja executar"
+$Label1.Location = New-Object System.Drawing.Point(250,30)
 $Label1.AutoSize = $true
 $Label1.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
 $font = New-Object System.Drawing.Font("Arial", 12, [System.Drawing.FontStyle]::Regular)
@@ -19,4 +25,44 @@ $Label1.Font = $font
 $main_form.Controls.Add($Label1)
 
 
+$Button1 = New-Object System.Windows.Forms.Button
+$Button1.Location = New-Object System.Drawing.Size(250,180)
+$Button1.Size = New-Object System.Drawing.Size(400,40)
+$Button1.Text = "fechar"
+$Button1.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$Button1.FlatAppearance.BorderColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$main_form.Controls.Add($Button1)
+
+
+
+$Button1.Add_Click(
+{
+    $main_form.Close()
+}
+
+)
+
+
+$Button2 = New-Object System.Windows.Forms.Button
+$Button2.Location = New-Object System.Drawing.Size(250,240)
+$Button2.Size = New-Object System.Drawing.Size(400,40)
+$Button2.Text = "fechar"
+$Button2.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$Button2.FlatAppearance.BorderColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$main_form.Controls.Add($Button2)
+
+
+
+$Button2.Add_Click(
+{
+    
+}
+
+)
+
+
 $main_form.ShowDialog()
+
+
+## Fim do Form inicio das funcoes
+
