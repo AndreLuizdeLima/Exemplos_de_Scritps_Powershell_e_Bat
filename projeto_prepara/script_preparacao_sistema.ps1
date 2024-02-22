2e6360244b6577ee9918b3aa7322b7c617b6410d
Andre <andreluizjp2015@gmail.com>
2024-02-21 22:21:41 -0300

    21022024

--- projeto_prepara/script_preparacao_sistema.ps1
+++ projeto_prepara/script_preparacao_sistema.ps1
@@ -1,18 +1,19 @@
 Add-Type -assembly System.Windows.Forms
 
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $arguments = "& '" + $MyInvocation.MyCommand.Definition + "'"
-    Start-Process powershell -Verb runAs -ArgumentList $arguments
-    exit
-}
+#se for necessario adm
+#if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+#    $arguments = "& '" + $MyInvocation.MyCommand.Definition + "'"
+#    Start-Process powershell -Verb runAs -ArgumentList $arguments
+#   exit
+#}
 
-$main_form = New-Object System.Windows.Forms.Form
-$main_form.Text ='Script de Utilitarios'
+$main_pricipal = New-Object System.Windows.Forms.Form
+$main_pricipal.Text ='Script de Utilitarios'
 
-$main_form.Width = 900
-$main_form.Height = 600
-$main_form.AutoSize = $true
-$main_form.BackColor = [System.Drawing.Color]::FromArgb(17, 17, 17)
+$main_pricipal.Width = 750
+$main_pricipal.Height = 500
+$main_pricipal.AutoSize = $true
+$main_pricipal.BackColor = [System.Drawing.Color]::FromArgb(17, 17, 17)
 
 $Label1 = New-Object System.Windows.Forms.Label
 $Label1.Text = "Selecione o que deseja executar"
@@ -22,46 +23,57 @@ $Label1.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
 $font = New-Object System.Drawing.Font("Arial", 12, [System.Drawing.FontStyle]::Regular)
 $Label1.Font = $font
 
-$main_form.Controls.Add($Label1)
+$main_pricipal.Controls.Add($Label1)
 
 
 $Button1 = New-Object System.Windows.Forms.Button
-$Button1.Location = New-Object System.Drawing.Size(250,180)
-$Button1.Size = New-Object System.Drawing.Size(400,40)
-$Button1.Text = "fechar"
+$Button1.Location = New-Object System.Drawing.Size(275,180)
+$Button1.Size = New-Object System.Drawing.Size(200,40)
+$Button1.Text = "Configurar"
 $Button1.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
 $Button1.FlatAppearance.BorderColor = [System.Drawing.Color]::FromArgb(217,255,255)
-$main_form.Controls.Add($Button1)
+$main_pricipal.Controls.Add($Button1)
 
 
 
 $Button1.Add_Click(
 {
-    $main_form.Close()
-}
+    $main_config = New-Object System.Windows.Forms.Form
+    $main_config.Text ='Configuracoes'
 
-)
+    $main_config.Width = 450    
+    $main_config.Height = 300
+    $main_config.AutoSize = $true
+    $main_config.BackColor = [System.Drawing.Color]::FromArgb(17, 17, 17)  
 
-
-$Button2 = New-Object System.Windows.Forms.Button
-$Button2.Location = New-Object System.Drawing.Size(250,240)
-$Button2.Size = New-Object System.Drawing.Size(400,40)
-$Button2.Text = "fechar"
-$Button2.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
-$Button2.FlatAppearance.BorderColor = [System.Drawing.Color]::FromArgb(217,255,255)
-$main_form.Controls.Add($Button2)
-
-
-
-$Button2.Add_Click(
-{
     
+
+    $main_config.ShowDialog()
+
 }
 
 )
 
 
-$main_form.ShowDialog()
+$Button_exit = New-Object System.Windows.Forms.Button
+$Button_exit.Location = New-Object System.Drawing.Size(275,240)
+$Button_exit.Size = New-Object System.Drawing.Size(200,40)
+$Button_exit.Text = "Fechar"
+$Button_exit.ForeColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$Button_exit.FlatAppearance.BorderColor = [System.Drawing.Color]::FromArgb(217,255,255)
+$main_pricipal.Controls.Add($Button_exit)
+
+
+
+$Button_exit.Add_Click(
+{
+    $main_pricipal.Close()
+}
+
+)
+
+
+$main_pricipal.ShowDialog()
 
 
 ## Fim do Form inicio das funcoes
