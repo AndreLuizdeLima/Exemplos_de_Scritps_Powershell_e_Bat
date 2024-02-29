be76a864abed6a80622818ce5acc910d8643a71b
Andre <andreluizjp2015@gmail.com>
2024-02-29 14:53:27 -0300

    29022024

---
+++ banco_de_dados.ps1
@@ -0,0 +1,43 @@
+﻿
+#Testando uma conexão de banco de dados
+
+
+Import-Module -Name "C:\Program Files (x86)\MySQL\MySQL Installer for Windows\MySQL.Data.dll"
+
+
+$server = "127.0.0.1"
+$database = "todolist"
+$username = "root"
+$password = "admin"
+
+
+$connectionString = "server=$server;port=3307;database=$database;uid=$username;pwd=$password;"
+
+
+$connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+$connection.ConnectionString = $connectionString
+
+
+$connection.Open()
+
+
+$query = "SELECT * FROM tasks;"
+$command = $connection.CreateCommand()
+$command.CommandText = $query
+$reader = $command.ExecuteReader()
+
+
+while ($reader.Read()) {
+    $result = ""
+    for ($i = 0; $i -lt $reader.FieldCount; $i++) {
+        $result += $reader.GetString($i) + " | "
+    }
+    Write-Host $result
+}
+
+
+$reader.Close()
+$connection.Close()
+
+
+
